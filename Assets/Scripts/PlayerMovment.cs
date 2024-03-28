3349772c8ac4138e3be5556646f60dbe40e85672
Gints <gintskristaps13@gmail.com>
2024-03-28 13:56:28 +0200

    Basic Camera And Movement

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    [Header("Keybinds")]
+
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Movement Settings")]
+    
+    public float movementSpeed;
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMovementMultiplier;
+    bool readyToJump;
+
+    [Header("References")]
+    public Transform orientation;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+
+    float horizontalInput;
+    float verticalInput;    
+    UnityEngine.Vector3 movementDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
+    }
+
+
+    private void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, UnityEngine.Vector3.down, playerHeight * 0.1f + 0.2f, whatIsGround);
+        Debug.DrawRay(transform.position, UnityEngine.Vector3.down * (playerHeight * 0.1f + 0.2f), Color.red);
+        GetInput();
+        SpeedControl();
+
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else 
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(jumpKey) && readyToJump && isGrounded)
+        {
+            Jump();
+            readyToJump = false;
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+    }
+
+    private void MovePlayer()
+    {
+        movementDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //log test text
+        Debug.Log("Test: " + isGrounded);
+        if (isGrounded)
+            rb.AddForce(movementDirection.normalized * movementSpeed* 10f, ForceMode.Force);
+        else if (!isGrounded)
+           rb.AddForce(movementDirection.normalized * movementSpeed * airMovementMultiplier, ForceMode.Force);
+    }
+    private void SpeedControl()
+    {
+        UnityEngine.Vector3 flatVelocity = new UnityEngine.Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        if (flatVelocity.magnitude > movementSpeed)
+        {
+            UnityEngine.Vector3 limitedVelocity = flatVelocity.normalized * movementSpeed;
+            rb.velocity = new UnityEngine.Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new UnityEngine.Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (readyToJump && isGrounded)
+        {
+            rb.AddForce(UnityEngine.Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
--- Assets/Scripts/ThirdPersonCam.cs
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -15,13 +15,29 @@ public class ThirdPersonCam : MonoBehaviour
 
     public float rotationSpeed;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
 
     private void Update()
     {
-        // //rotate orientation
-        // UnityEngine.Vector3 viewDir = player.position - new UnityEngine.Vector3(transform.position.x, transform.position.y, transform.position.z);
-        // orientation.forward = viewDir.normalized;
+        UnityEngine.Vector3 viewDir = player.position - new UnityEngine.Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        UnityEngine.Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        if(inputDir != UnityEngine.Vector3.zero)
+        {
+            playerObj.forward = UnityEngine.Vector3.Slerp(playerObj.forward, inputDir, rotationSpeed * Time.deltaTime);
+        }
+        
+    
     }
 
 }
