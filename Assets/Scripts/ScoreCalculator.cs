58eb3b9c0097f3b71d4925f4059c0844b1a4c0b3
Gints <gintskristaps13@gmail.com>
2024-05-10 12:33:14 +0300

    update

--- Assets/Scripts/ScoreCalculator.cs
+++ Assets/Scripts/ScoreCalculator.cs
@@ -8,23 +8,39 @@ public class ScoreCalculator : MonoBehaviour
 {
     private GameManager gameManager;
     private Rigidbody rigidbody;
+    private int objectSettleTime = 5; //s
+    private bool isSettling = true;
     private float currentScore = 0;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(SettleTime());
         rigidbody = GetComponent<Rigidbody>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
+
+    IEnumerator SettleTime()
+    {
+        yield return new WaitForSeconds(objectSettleTime);
+        isSettling = false;
+    }
+
+
     void FixedUpdate()
     {
+        
         if (rigidbody != null)
         {
             bool isSleeping = rigidbody.IsSleeping();
-
+            if (isSettling)
+            {
+                return;
+            }
             if (isSleeping && currentScore > 0)
             {
                 gameManager.addScore((int)currentScore);
+            
                 currentScore = 0;
             }
             else
